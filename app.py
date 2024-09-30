1a48399d1525ae9dcb5faf04adf9233b4542e0aa
Clement Boiteux <67805925+cboiteux2765@users.noreply.github.com>
2024-09-30 00:39:16 -0700

    refactor function for image later

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-import gradio as gr
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from PIL import Image
+from diffusers import StableDiffusionImg2ImgPipeline
+import gradio as gr
+import torch
+import json
+import time
 
 # Load your custom Hugging Face model
 model_id = "vikhyatk/moondream2"
@@ -10,13 +14,17 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-def respond(image):
+def generate_text(image):
     prompt = "This room seems messy. How can I organize the items?"
 
     enc_image = model.encode_image(image)
     response = model.answer_question(enc_image, prompt, tokenizer)
 
     return response
+    
+def respond(image):
+   text = generate_text(image)
+   return text
 
 # Create the Gradio interface
 demo = gr.Interface(
